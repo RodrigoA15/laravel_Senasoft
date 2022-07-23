2617b1137f9297670b8b1969937ca81b28df1fa9
unknown <rapapamija@misena.edu.co>
2022-07-23 13:10:11 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\RegistroController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
